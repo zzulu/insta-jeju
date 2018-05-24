cf9eb2c9ff0c465cd0e833abced71aedb9b686b8
Junwoo Hwang <capollux10@gmail.com>
2018-05-24 07:23:29 +0000

    Add mypage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,6 +63,10 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def mypage
+    @posts = current_user.posts
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations:'users/registrations'}
   root 'posts#index'
   resources :posts
+  get 'mypage', to: 'posts#mypage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
