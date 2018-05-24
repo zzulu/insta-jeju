15f152b87f8abada141374b7266a3bafe729cffe
Junwoo Hwang <capollux10@gmail.com>
2018-05-24 07:21:21 +0000

    Add rolify config

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
     can :read, :all
     if user.present?
       can :manage, Post, user_id: user.id
+      if user.has_role?(:admin)
+        can :manage, :all
+      end
     end
     
     # Define abilities for the passed in user here. For example:
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +8,10 @@ class User < ApplicationRecord
   has_many :posts
   
   validates :name, presence: true
+  
+  after_create :default_role
+  
+  def default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 end
